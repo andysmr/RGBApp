effa9224c703177b96f01db1631199dfc6bb27d6
andySmr <andrey180696007@yandex.ru>
2021-12-26 15:13:27 +0400

    version 1.6

--- RGBApp/ViewController.swift
+++ RGBApp/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
     @IBOutlet var mainView: UIView!
     @IBOutlet var redNumber: UILabel!
@@ -18,20 +17,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layer.cornerRadius = 20
+        redSlider.minimumTrackTintColor = .red
+        redSlider.thumbTintColor = .systemGray4
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.thumbTintColor = .systemGray4
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.thumbTintColor = .systemGray4
         // Do any additional setup after loading the view.
+    }
+    func colorView() {
+        mainView.backgroundColor = UIColor(red: CGFloat(Int(redSlider.value))/255, green: CGFloat(Int(greenSlider.value))/255, blue: CGFloat(Int(blueSlider.value))/255, alpha: 1)
     }
 
     @IBAction func redSliderActivation() {
         redNumber.text = String(Int(redSlider.value))
-        mainView.backgroundColor = UIColor(red: CGFloat(Int(redSlider.value))/255, green: CGFloat(Int(greenSlider.value))/255, blue: CGFloat(Int(blueSlider.value))/255, alpha: 1)
+        colorView()
     }
     @IBAction func greenSliderActivation() {
         greenNumber.text = String(Int(greenSlider.value))
-        mainView.backgroundColor = UIColor(red: CGFloat(Int(redSlider.value))/255, green: CGFloat(Int(greenSlider.value))/255, blue: CGFloat(Int(blueSlider.value))/255, alpha: 1)
+        colorView()
     }
     @IBAction func blueSliderActivation() {
         blueNumber.text = String(Int(blueSlider.value))
-        mainView.backgroundColor = UIColor(red: CGFloat(Int(redSlider.value))/255, green: CGFloat(Int(greenSlider.value))/255, blue: CGFloat(Int(blueSlider.value))/255, alpha: 1)
+        colorView()
     }
 }
 
